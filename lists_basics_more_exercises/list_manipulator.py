3d6c6babf9a84056b1f66c3dd8a7a51fb31e891f
Medo0301 <m.hodzhov0301@gmail.com>
2026-06-05 12:36:29 +0300

    tic tac toe and list manipulator

--- lists_basics_more_exercises/list_manipulator.py
+++ lists_basics_more_exercises/list_manipulator.py
@@ -5,18 +5,62 @@ while True:
     if command == "end":
         print(list_of_numbers)
         break
-    elif "exchange" in command:
+    if "even" in command:
+        target_rem = 0
+    else:
+        target_rem = 1
+    if "exchange" in command:
         index = int(command.split()[1])
         if 0 <= index < len(list_of_numbers):
-            pass
+            list_of_numbers = list_of_numbers[index + 1:] + list_of_numbers[:index + 1]
         else:
             print("Invalid index")
     elif "max" in command:
-        pass
+        max_value = None
+        max_value_idx = -1
+        for idx in range(len(list_of_numbers)):
+            if list_of_numbers[idx] % 2 == target_rem and \
+                    (max_value is None or list_of_numbers[idx] >= max_value):
+                max_value = list_of_numbers[idx]
+                max_value_idx = idx
+        if max_value_idx != -1:
+            print(max_value_idx)
+        else:
+            print("No matches")
     elif "min" in command:
-        pass
+        min_value = None
+        min_value_idx = -1
+        for idx in range(len(list_of_numbers)):
+            if list_of_numbers[idx] % 2 == target_rem and \
+                    (min_value is None or list_of_numbers[idx] <= min_value):
+                min_value = list_of_numbers[idx]
+                min_value_idx = idx
+        if min_value_idx != -1:
+            print(min_value_idx)
+        else:
+            print("No matches")
     elif "first" in command:
-        pass
-    elif "last" in command:
-        pass
+        count = int(command.split()[1])
+        if count > len(list_of_numbers):
+            print("Invalid count")
+            continue
+        first_target_elements = []
+        for num in list_of_numbers:
+            if num % 2 == target_rem:
+                first_target_elements.append(num)
+                if len(first_target_elements) == count:
+                    break
+        print(first_target_elements)
 
+    elif "last" in command:
+        count = int(command.split()[1])
+        if count > len(list_of_numbers):
+            print("Invalid count")
+            continue
+        last_target_elements = []
+        for idx in range(len(list_of_numbers) - 1, -1, -1):
+            if list_of_numbers[idx] % 2 == target_rem:
+                last_target_elements.insert(0, list_of_numbers[idx])
+                if len(last_target_elements) == count:
+                    break
+        print(last_target_elements)
--- lists_basics_more_exercises/tic_tac_toe.py
+++ lists_basics_more_exercises/tic_tac_toe.py
@@ -1,4 +1,22 @@
-first_line = input().split()
-second_line = input().split()
-third_line = input().split()
+row_1 = input().split()
+row_2 = input().split()
+row_3 = input().split()
 
+winning_combo =[[row_1[0], row_1[1], row_1[2]],
+                [row_2[0], row_2[1], row_2[2]],
+                [row_3[0], row_3[1], row_3[2]],
+                [row_1[0], row_2[0], row_3[0]],
+                [row_1[1], row_2[1], row_3[1]],
+                [row_1[2], row_2[2], row_3[2]],
+                [row_1[0], row_2[1], row_3[2]],
+                [row_1[2], row_2[1], row_3[0]]]
+
+for combo in winning_combo:
+    if combo[0] == combo[1] == combo[2] == '1':
+        print("First player won")
+        break
+    elif combo[0] == combo[1] == combo[2] == '2':
+        print("Second player won")
+        break
+else:
+    print("Draw!")
