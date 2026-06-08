1091cb7ec3e99b2e6539a7b48b317a87303fad52
Medo0301 <m.hodzhov0301@gmail.com>
2026-06-08 15:50:33 +0300

    even_number

--- functions_exercise/even_numbers.py
+++ functions_exercise/even_numbers.py
@@ -1,7 +1,8 @@
 list_numbers = input().split()
+list_int_numbers = []
 for idx in range(len(list_numbers)):
-    list_numbers[idx] = int(list_numbers[idx])
+    list_int_numbers[idx] = int(list_numbers[idx])
 
-even_numbers = list(filter(lambda num: num % 2 == 0, list_numbers))
+even_numbers = list(filter(lambda num: num % 2 == 0, list_int_numbers))
 
 print(even_numbers)
